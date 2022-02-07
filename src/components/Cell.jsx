3fa093890de6b8a06ca6d3cedb7368cf259b4bbf
David de Lamo <d.lamo.valverde@gmail.com>
2022-02-07 12:37:11 +0100

    add img

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,6 +1,6 @@
 export default function Cell({idx, state}) {
   const isGroguHere = idx === state.position
   return <div className="cell">
-    <p style={{display: isGroguHere ? 'block' : 'none'}}>Grogu</p>
+    <img src="/images/grogu.jpg" style={{display: isGroguHere ? 'block' : 'none'}} alt='Grogu' />
   </div>
 }
